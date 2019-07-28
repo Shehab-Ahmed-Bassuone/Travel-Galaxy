b579983497919ba2579b5d4a4be29038fd5a9ef1
ahmedbeeka <ahmed.abdelgawad.farag@gmail.com>
2019-07-28 19:21:25 +0200

    fix bug

--- php/adminpackages.php
+++ php/adminpackages.php
@@ -25,14 +25,17 @@ if ( isset( $_POST['button1'] ) ) {
     $stmt->execute();
     $stmt->close();
     $conn->close();
-
+    echo "<script> alert(\" card changed successfully \"); 
+    location.replace('../packages.php');
+    
+    </script>";
 
 
 }else if(isset( $_POST['button2'] )){
     $destination=$_POST['destination2'];
     $duration=$_POST['duration2'];
     $cost=$_POST['cost2'];
-    $INSERT = "INSERT Into places destination=?,duration=?,cost=? values( ?, ?,?) WHERE id = 2";
+    $INSERT = "UPDATE places SET destination=?,duration=?,cost=?  WHERE id = 2";
     $stmt = $conn->prepare($INSERT);
     
    
@@ -40,14 +43,17 @@ if ( isset( $_POST['button1'] ) ) {
     $stmt->execute();
     $stmt->close();
     $conn->close();
-
+    echo "<script> alert(\" card changed successfully \"); 
+    location.replace('../packages.php');
+    
+    </script>";
 
 }
 else if(isset( $_POST['button3'] )){
     $destination=$_POST['destination3'];
     $duration=$_POST['duration3'];
     $cost=$_POST['cost3'];
-    $INSERT = "INSERT Into places destination=?,duration=?,cost=? values( ?, ?,?) WHERE id = 3";
+    $INSERT = "UPDATE places SET destination=?,duration=?,cost=?  WHERE id = 3";
     $stmt = $conn->prepare($INSERT);
     
    
@@ -55,18 +61,25 @@ else if(isset( $_POST['button3'] )){
     $stmt->execute();
     $stmt->close();
     $conn->close();
+    echo "<script> alert(\" card changed successfully \"); 
+            location.replace('../packages.php');
+            
+            </script>";
 
 }
 else if(isset( $_POST['button4'] )){
     $destination=$_POST['destination4'];
     $duration=$_POST['duration4'];
     $cost=$_POST['cost4'];
-    $INSERT = "INSERT Into places destination=?,duration=?,cost=? values( ?, ?,?) WHERE id = 4";
+    $INSERT = "UPDATE places SET destination=?,duration=?,cost=? values( ?, ?,?) WHERE id = 4";
     $stmt->bind_param("sii", $destination, $duration, $cost);
     $stmt->execute();
     $stmt->close();
     $conn->close();
-
+    echo "<script> alert(\" card changed successfully \"); 
+            location.replace('../packages.php');
+            
+            </script>";
 
 
 }
@@ -74,45 +87,59 @@ else if(isset( $_POST['button5'] )){
     $destination=$_POST['destination5'];
     $duration=$_POST['duration5'];
     $cost=$_POST['cost5'];
-    $INSERT = "INSERT Into places destination=?,duration=?,cost=? values( ?, ?,?) WHERE id = 5";
+    $INSERT = "UPDATE places SET destination=?,duration=?,cost=?  WHERE id = 5";
     $stmt->bind_param("sii", $destination, $duration, $cost);
     $stmt->execute();
     $stmt->close();
     $conn->close();
+    echo "<script> alert(\" card changed successfully \"); 
+    location.replace('../packages.php');
+    
+    </script>";
 
 }
 else if(isset( $_POST['button6'] )){
     $destination=$_POST['destination6'];
     $duration=$_POST['duration6'];
     $cost=$_POST['cost6'];
-    $INSERT = "INSERT Into places destination=?,duration=?,cost=? values( ?, ?,?) WHERE id = 6";
+    $INSERT = "UPDATE places SET destination=?,duration=?,cost=?  WHERE id = 6";
     $stmt->bind_param("sii", $destination, $duration, $cost);
     $stmt->execute();
     $stmt->close();
     $conn->close();
+    echo "<script> alert(\" card changed successfully \"); 
+    location.replace('../packages.php');
+    
+    </script>";
 
 }
 else if(isset( $_POST['button7'] )){
     $destination=$_POST['destination7'];
     $duration=$_POST['duration7'];
     $cost=$_POST['cost7'];
-    $INSERT = "INSERT Into places destination=?,duration=?,cost=? values( ?, ?,?) WHERE id = 7";
+    $INSERT = "UPDATE places SET destination=?,duration=?,cost=?  WHERE id = 7";
     $stmt->bind_param("sii", $destination, $duration, $cost);
     $stmt->execute();
     $stmt->close();
     $conn->close();
-
+    echo "<script> alert(\" card changed successfully \"); 
+    location.replace('../packages.php');
+    
+    </script>";
 }
 else if(isset( $_POST['button8'] )){
     $destination=$_POST['destination8'];
     $duration=$_POST['duration8'];
     $cost=$_POST['cost8'];
-    $INSERT = "INSERT Into places destination=?,duration=?,cost=? values( ?, ?,?) WHERE id = 8";
+    $INSERT = "UPDATE places SET destination=?,duration=?,cost=?  WHERE id = 8";
     $stmt->bind_param("sii", $destination, $duration, $cost);
     $stmt->execute();
     $stmt->close();
     $conn->close();
-
+    echo "<script> alert(\" card changed successfully \"); 
+    location.replace('../packages.php');
+    
+    </script>";
 }
 
 
